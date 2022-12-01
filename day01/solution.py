7db90738748645a2eb1969c487f09b82f0382c4f
Jakob <jakobedvarderlandsson@gmail.com>
2022-12-01 16:25:43 +0100

    redid solution featuring numpy

--- day01/solution.py
+++ day01/solution.py
@@ -1,19 +1,20 @@
 from os import getenv
+import numpy as np
 
 
 class Solution:
 
     def __init__(self):
-        input_file = open("input.txt", 'r').read()
-        self.calorie_count = [map(int, r.split('\n')) for r in input_file.split('\n\n')]
+        input_file = open('input.txt', 'r').read()
+        self.calorie_count = [np.array(r.split('\n')).astype(int) for r in input_file.split('\n\n')]
 
-    def calculate_calories(self) -> list[int]:
-        return [sum(cals) for cals in self.calorie_count]
+    def calculate_calories(self):
+        return np.array([a.sum() for a in self.calorie_count])
 
 
 if __name__ == '__main__':
     sol = Solution()
     match getenv('part'):
-        case 'part1': print(max(sol.calculate_calories()))
-        case 'part2': print(sum(sorted(sol.calculate_calories())[-3:]))
+        case 'part1': print(np.max(sol.calculate_calories()))
+        case 'part2': print(np.sort(sol.calculate_calories())[-3:].sum())
         case _:       exit('Invalid name of environment variable \'part\'')
