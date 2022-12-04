09f2c17564c975dc7b9a6aa60d7dd63288d9007d
Jakob <jakobedvarderlandsson@gmail.com>
2022-12-04 17:06:18 +0100

    Complete day 4: Camp Cleanup

---
+++ day04/solution.py
@@ -0,0 +1,31 @@
+from os import getenv
+
+
+class Solution:
+
+    def __init__(self):
+        input_file = open('input.txt', 'r').read()
+        input_file = [a.split(',') for a in input_file.split('\n')]
+        self.pairs = []
+        for p in input_file:
+            (elf1, elf2) = p
+            elf1 = list(map(int, elf1.split('-')))
+            elf2 = list(map(int, elf2.split('-')))
+            self.pairs.append((elf1, elf2))
+
+    def calc(self, part: str) -> int:
+        num = 0
+        for p in self.pairs:
+            (elf1, elf2) = p
+            ranges = (range(elf1[0], elf1[1]+1), range(elf2[0], elf2[1]+1))
+            inter = set(ranges[0]).intersection(ranges[1])
+            if part == 'part1' and inter.__len__() in map(len, ranges):
+                num += 1
+            if part == 'part2' and inter:
+                num += 1
+        return num
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.calc(getenv('part')))
