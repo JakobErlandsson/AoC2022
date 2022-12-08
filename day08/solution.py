1eecd1446efb251f1f825de053a12fd870b7e6f1
Jakob <jakobedvarderlandsson@gmail.com>
2022-12-08 14:40:21 +0100

    clean up

--- day08/solution.py
+++ day08/solution.py
@@ -1,16 +1,16 @@
+import itertools
 from os import getenv
-from time import time
-import numpy as np
+from math import prod
 
 
 def get_neighbours(size):
     neighbour_map = {}
-    for y in range(1, size[0]-1):
-        for x in range(1, size[1]-1):
+    for y in range(1, size-1):
+        for x in range(1, size-1):
             left = [(i, y) for i in range(x)]
-            right = [(i, y) for i in range(x+1, size[0])]
+            right = [(i, y) for i in range(x+1, size)]
             up = [(x, j) for j in range(y)]
-            down = [(x, j) for j in range(y+1, size[1])]
+            down = [(x, j) for j in range(y+1, size)]
             neighbour_map[(x, y)] = [left, right, up, down]
 
     return neighbour_map
@@ -20,16 +20,16 @@ class Solution:
 
     def __init__(self):
         trees = [list(map(int, list(l))) for l in open('input.txt', 'r').read().split('\n')]
-        self.size = (len(trees), len(trees))
-        self.tree_map = {(x, y): trees[y][x] for x in range(self.size[0]) for y in range(self.size[1])}
+        self.size = len(trees)
+        self.tree_map = {(x, y): trees[y][x] for x in range(self.size) for y in range(self.size)}
         self.neighbours = {}
 
     def look_from_outside(self):
         seen = 0
         self.neighbours = get_neighbours(self.size)
-        for y in range(self.size[0]):
-            for x in range(self.size[1]):
-                if y in (0, self.size[0]-1) or x in (0, self.size[1]-1):
+        for y in range(self.size):
+            for x in range(self.size):
+                if y in (0, self.size-1) or x in (0, self.size-1):
                     seen += 1
                 else:
                     neighbours = self.neighbours[(x, y)]
@@ -59,7 +59,7 @@ class Solution:
         seen_list.append(seen)
         seen = 0
         # Look down (increase y)
-        for i in range(y+1, self.size[1]):
+        for i in range(y+1, self.size):
             seen += 1
             tree = self.tree_map[(x, i)]
             if tree >= height:
@@ -67,14 +67,14 @@ class Solution:
         seen_list.append(seen)
         seen = 0
         # Look right (increase x)
-        for i in range(x+1, self.size[1]):
+        for i in range(x+1, self.size):
             seen += 1
             tree = self.tree_map[(i, y)]
             if tree >= height:
                 break
         seen_list.append(seen)
 
-        return np.prod(seen_list)
+        return prod(seen_list)
 
 
 if __name__ == '__main__':
@@ -83,7 +83,7 @@ if __name__ == '__main__':
         case 'part1':
             print(sol.look_from_outside())
         case 'part2':
-            scores = [sol.find_scenic_score(x, y) for y in range(1, sol.size[0]-1) for x in range(1, sol.size[1]-1)]
+            scores = [sol.find_scenic_score(x, y) for y in range(1, sol.size-1) for x in range(1, sol.size-1)]
             print(max(scores))
 
 
